de411fcc95662e343d74c72006308f7ba740025c
Evgeny Karasev <gnomskg@gmail.com>
2017-04-19 00:56:29 +0300

    Added lift up local RangeFilter state to Filter state

--- src/components/RangeFilter/index.jsx
+++ src/components/RangeFilter/index.jsx
@@ -2,24 +2,33 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function RangeFilter({options, onChange}) {
-  const {unit} = options;
+  function modifyBound(inputValue, type) {
+    return (prevValue) => {
+      const boundValue = parseInt(inputValue, 10);
+      if (isNaN(boundValue)) {
+        return prevValue
+          .map((pare) => Object.assign([], pare))
+          .filter((pare) => !Object.is(pare[0], type));
+      }
+      return prevValue
+        .map((pare) => Object.assign([], pare))
+        .filter((pare) => !Object.is(pare[0], type))
+        .concat([[type, boundValue]]);
+    }
+  }
   return (
     <div className="blockSearch_quantity">
       <span>от</span>
-      <input type="text" />
+      <input type="text" onChange={(e) => onChange(modifyBound(e.target.value, "lowBound"))} />
       <span>до</span>
-      <input type="text" />
-      <span>{unit}</span>
+      <input type="text" onChange={(e) => onChange(modifyBound(e.target.value, "upperBound"))} />
+      <span>{options.unit}</span>
     </div>
   )
 }
 
 RangeFilter.propTypes = {
-  unit: PropTypes.string
-};
-
-RangeFilter.defaultProps = {
-  unit: "у.е."
+  options: PropTypes.object.isRequired
 };
 
 export default RangeFilter;
